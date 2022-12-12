038d4106330f263d9e57ee3e531a80003653cea0
J. Ring-Sakabe <jordanringsakabe@gmail.com>
2022-12-12 11:29:43 -0700

    completed lesson 34

--- the_shuppi_tracker/src/components/ExpenseItem.js
+++ the_shuppi_tracker/src/components/ExpenseItem.js
@@ -1,10 +1,12 @@
+import "./ExpenseItem.css";
+
 function ExpenseItem() {
   return (
-    <div>
-      <div>March 28th 2021</div>
-      <div>
-        <h2>Car Insurance</h2>
-        <div>$249.67</div>
+    <div className="expense-item">
+      <div className="">March 28th 2021</div>
+      <div className="expense-item__description">
+        <h2 className="">Car Insurance</h2>
+        <div className="expense-item__price">$249.67</div>
       </div>
     </div>
   );
