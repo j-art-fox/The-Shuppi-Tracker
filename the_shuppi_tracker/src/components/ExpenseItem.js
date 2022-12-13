9ff6cb38855812977058959111921a20f0680c79
J. Ring-Sakabe <jordanringsakabe@gmail.com>
2022-12-13 09:51:41 -0700

    completed 35

--- the_shuppi_tracker/src/components/ExpenseItem.js
+++ the_shuppi_tracker/src/components/ExpenseItem.js
@@ -1,12 +1,15 @@
 import "./ExpenseItem.css";
 
 function ExpenseItem() {
+  const expenseDate = new Date(2021, 2, 28);
+  const expenseTitle = "Car Insurance";
+  const expenseAmount = 294.67;
   return (
     <div className="expense-item">
-      <div className="">March 28th 2021</div>
+      <div className="">{expenseDate.toISOString()}</div>
       <div className="expense-item__description">
-        <h2 className="">Car Insurance</h2>
-        <div className="expense-item__price">$249.67</div>
+        <h2 className="">{expenseTitle}</h2>
+        <div className="expense-item__price">${expenseAmount}</div>
       </div>
     </div>
   );
