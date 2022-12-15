9dacde7f2695377022da629cad7671a407733dfe
J. Ring-Sakabe <jordanringsakabe@gmail.com>
2022-12-14 23:55:22 -0700

    debbuged

--- the_shuppi_tracker/src/App.js
+++ the_shuppi_tracker/src/App.js
@@ -4,23 +4,27 @@ function App() {
   const expenses = [
     {
       title: "Car Insurance",
-      amount: Math.random() * 10,
+      amount: 100,
       date: new Date(2021, 2, 28),
     },
     {
       title: "Monkies",
-      amount: Math.random() * 10,
+      amount: 100,
       date: new Date(2021, 2, 28),
     },
-    { title: "Pens", amount: Math.random() * 10, date: new Date(2021, 2, 28) },
+    {
+      title: "Pens",
+      amount: 100,
+      date: new Date(2021, 2, 28),
+    },
     {
       title: "Pencils",
-      amount: Math.random() * 10,
+      amount: 100,
       date: new Date(2021, 2, 28),
     },
     {
       title: "Puppies",
-      amount: Math.random() * 10,
+      amount: 100,
       date: new Date(2021, 2, 28),
     },
   ];
@@ -31,6 +35,21 @@ function App() {
         title={expenses[0].title}
         amount={expenses[0].amount}
         date={expenses[0].date}
+      />
+      <ExpenseItem
+        title={expenses[1].title}
+        amount={expenses[1].amount}
+        date={expenses[1].date}
+      />
+      <ExpenseItem
+        title={expenses[2].title}
+        amount={expenses[2].amount}
+        date={expenses[2].date}
+      />
+      <ExpenseItem
+        title={expenses[3].title}
+        amount={expenses[3].amount}
+        date={expenses[3].date}
       />
     </div>
   );
